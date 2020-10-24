bd118d2a6c22c241972eafd55b22b41f1fa9eb51
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2020-10-25 01:11:40 +0530

    Update sparse.cpp
    sparse matrix updated!!

--- sparse.cpp
+++ sparse.cpp
@@ -28,6 +28,17 @@ public:
         cin>>nums;
         ele=new element[nums];
     }
+    sparse (int n,int m,int nums)
+    {
+        this->n=n;
+        this->m=m;
+        this->nums=nums;
+        ele=new element[nums];
+    }
+    ~sparse()
+    {
+        delete ele[];
+    }
     void read()
     {
         cout<<endl<<"Enter elements : "<<endl;
@@ -44,7 +55,7 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(i==ele[k].row && j==ele[k].col)
+                if(k<nums && i==ele[k].row && j==ele[k].col)
                 {
                     cout<<ele[k++].val<<" ";
                 }
@@ -56,12 +67,55 @@ public:
             cout<<endl;
         }
     }
+    sparse* add(sparse obj)
+    {
+        sparse *ans=new sparse(obj.n,obj.m,obj.nums+nums);
+        int k=0;
+        int i=0;
+        int j=0;
+        while(i<obj.nums && j<nums)
+        {
+            if(ele[i].row<obj.ele[j].row)
+            {
+                ans->ele[k++]=ele[i++];
+            }
+            else if(ele[i].row>obj.ele[j].row)
+            {
+                ans->ele[k++]=obj.ele[j++];
+            }
+            else
+            {
+                if(ele[i].col<obj.ele[j].col)
+                {
+                    ans->ele[k++]=ele[i++];
+                }
+                else if(ele[i].col>obj.ele[j].col)
+                {
+                    ans->ele[k++]=obj.ele[j++];
+                }
+                else
+                {
+                    ans->ele[k]=ele[i];
+                    ans->ele[k++].val=ele[i++].val+obj.ele[j++].val;
+                }
+            }
+        }
+        for(;i<nums;) ans->ele[k++]=ele[i++];
+        for(;j<obj.nums;) ans->ele[k++]=obj.ele[j++];
+        ans->nums=k;
+        return ans;
+    }
 };
 
 int main()
 {
-    sparse k;
+    sparse k,m;
+    sparse *c;
     k.read();
+    m.read();
+    c=k.add(m);
     k.display();
+    m.display();
+    c->display();
     return 0;
 }
